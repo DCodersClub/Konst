9109f5c4e7d64b0b09cc039f9f5339b4f906596a
purry03 <rohansh.lko@gmail.com>
2021-04-08 22:56:51 +0530

    removed bullshit from backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,9 @@ const { ensureAuthenticated } = require("../config/auth");
 // Get: Returns home page
 router.get("/", async (req, res) => {
   if (req.isAuthenticated()) {
-    res.render("index.ejs", { user: req.user, page: 0 });
+    res.render("index.ejs", { user: req.user});
   } else {
-    res.render("index.ejs", { page: 0 });
+    res.render("index.ejs");
   }
 });
 
