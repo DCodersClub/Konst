e02ac22625f4b163e1aa43e884f97c45462b527a
purry03 <rohansh.lko@gmail.com>
2021-04-08 14:10:55 +0530

    added login

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,51 +1,70 @@
-const User = require('../models/user');
-const router = require('express').Router();
-const bcrypt=require('bcryptjs');
+const User = require("../models/user");
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-router.get("/login",async(req,res)=>{
-    res.render("user/login.ejs");
+router.get("/login", async (req, res) => {
+  res.render("user/login.ejs");
 });
 
-router.post("/login",async(req,res)=>{
-    console.log("Login Post handle");
-});
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-router.get("/register",async(req,res)=>{
-    res.render("user/register.ejs");
-});
-
-router.post("/register",async(req,res)=>{
-    let errors=[];
-    const{name, email, mobileNumber, password,collegeName}=req.body;
-    console.log(req.body);
-
-    //Check if user already exist with this email
-    //Validations will go here.
-
-    const saltRounds = 10;
-	const hashPass = await bcrypt.hash(password,saltRounds);
-    let newUser=User({
-        name:name,
-        email:email,
-        mobileNumber:mobileNumber,
-        password: hashPass,
-        collegeName: collegeName,
+  User.findOne({ email: email })
+    .then((user) => {
+      if (user == null) {
+        res.send("you aint gettin in homie");
+      } else {
+        if (bcrypt.compareSync(password, user.password)) {
+          res.send("logged in");
+        } else {
+          res.send("still not getting in");
+        }
+      }
     })
-    try{
-        const user=await newUser.save();
+    .catch((err) => {
+      res.send("server's fucked up mate");
+    });
+});
+
+router.get("/register", async (req, res) => {
+  res.render("user/register.ejs");
+});
+
+router.post("/register", async (req, res) => {
+  let errors = [];
+  const { name, email, mobileNumber, password, collegeName } = req.body;
+  console.log(req.body);
+
+  //Check if user already exist with this email
+  //Validations will go here.
+
+  const saltRounds = 10;
+  const hashPass = await bcrypt.hash(password, saltRounds);
+
+  let newUser = User({
+    name: name,
+    email: email,
+    mobileNumber: mobileNumber,
+    password: hashPass,
+    collegeName: collegeName,
+  });
+  try {
+    newUser
+      .save()
+      .then(() => {
         res.redirect("/user/login");
-    }
-    catch(e){
-        console.log(e);
-    }
+      })
+      .catch((err) => {
+        res.send("shit");
+      });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
-
-
-
-router.get("/all",async(req,res)=>{
-    users=await User.find({});
-    res.json(users);
+router.get("/all", async (req, res) => {
+  users = await User.find({});
+  res.json(users);
 });
 
-module.exports=router;
+module.exports = router;
