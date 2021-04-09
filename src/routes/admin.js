cf91ba8cd34b5bcd43f9d35c864111feb05c360e
purry03 <rohansh.lko@gmail.com>
2021-04-09 22:59:27 +0530

    minor changes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -37,7 +37,7 @@ router.post("/login", async (req, res, next) => {
   let { username, password } = req.body;
   if ((username = "admin" && password == "password")) {
     res.cookie("authed", "true");
-    res.send("logged in");
+    res.redirect("admin/dashboard");
   }
 });
 
