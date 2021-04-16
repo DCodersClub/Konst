c9b19dce1ebdf838a7239cc5519dd452ead98987
purry03 <rohansh.lko@gmail.com>
2021-04-16 10:51:53 +0530

    passport local

--- src/loaders/mongoose-loader.js
+++ src/loaders/mongoose-loader.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 module.exports = async () => {
-	let connURL = "mongodb+srv://loneCoder:QWERTY1234@cluster0-avdpm.mongodb.net/konstDB?retryWrites=true&w=majority";
-    await mongoose.connect(connURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    });
+  let connURL =
+    "mongodb+srv://loneCoder:QWERTY1234@cluster0-avdpm.mongodb.net/konstDB?retryWrites=true&w=majority";
+  await mongoose.connect(connURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+  mongoose.plugin(passportLocalMongoose);
 };
