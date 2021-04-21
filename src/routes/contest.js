b51630e962d04e63597acc0c5dcca78a51396c87
purry03 <rohansh.lko@gmail.com>
2021-04-21 18:33:15 +0530

    test

--- src/routes/contest.js
+++ src/routes/contest.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 const passport = require("passport");
 const { mongo } = require("mongoose");
 const { ensureAuthenticated } = require("../config/auth");
-const start = new Date("April 30, 2021 12:00:00 GMT+0530");
+const start = new Date("April 3, 2021 12:00:00 GMT+0530");
 const end = new Date("April 30, 2021 18:00:00 GMT+0530");
 
 router.get("/", ensureAuthenticated, async (req, res) => {
